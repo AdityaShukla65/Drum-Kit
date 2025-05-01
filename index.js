c1eceb814be35f14ceb040e86a3e3bf4ca447e17
AdityaShukla65 <102731870+AdityaShukla65@users.noreply.github.com>
2025-05-01 14:31:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const lst=["w","a","s","d","j","k","l"]
 const soundMap={
-    w:"/Drum-Kit/Drum Kit Starting Files/sounds/crash.mp3",
-    a:"/Drum-Kit/Drum Kit Starting Files/sounds/kick-bass.mp3",
-    s:"/Drum-Kit/Drum Kit Starting Files/sounds/snare.mp3",
-    d:"/Drum-Kit/Drum Kit Starting Files/sounds/tom-1.mp3",
-    j:"/Drum-Kit/Drum Kit Starting Files/sounds/tom-2.mp3",
-    k:"/Drum-Kit/Drum Kit Starting Files/sounds/tom-3.mp3",
-    l:"/Drum-Kit/Drum Kit Starting Files/sounds/tom-4.mp3"
+    w:"crash.mp3",
+    a:"kick-bass.mp3",
+    s:"snare.mp3",
+    d:"tom-1.mp3",
+    j:"tom-2.mp3",
+    k:"tom-3.mp3",
+    l:"tom-4.mp3"
 }
 
 function playSound(key){
